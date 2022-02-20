562c77faa112bf4441819c4691e81b06c13b9dc1
Thowa92 <thomas.waljor@outlook.de>
2022-02-20 22:01:00 +0100

    Remove todos (will not implement relevant changes due to time constraints)

--- src/main/java/com/example/urlshortener/controller/UrlShortenerController.java
+++ src/main/java/com/example/urlshortener/controller/UrlShortenerController.java
@@ -47,8 +47,6 @@ public class UrlShortenerController {
 	 */
 	@GetMapping("/{urlShortExtension}")
 	ResponseEntity<Object> redirect(@PathVariable String urlShortExtension) {
-		// TODO get Domain without constant?
-		// TODO only return URL and let the frontend handle the redirect
 		URI uri = URI.create(urlShortenerService.loadUrlLongByUrlShort(DOMAIN + urlShortExtension, DOMAIN, true));
 		return ResponseEntity.status(HttpStatus.FOUND).location(uri).build();
 	}
